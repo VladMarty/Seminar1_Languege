dc47e640ddaef8d692123f9286e80147d2c41cf9
Vlad <efimovich94@inbox.ru>
2023-01-15 17:06:57 +0300

    Задача №1

---
+++ Example/Example003/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA == numB * numB)
+{
+    Console.WriteLine("Числа равны!");
+
+}
+else
+{
+    Console.WriteLine("Числа не равны!");
+}
