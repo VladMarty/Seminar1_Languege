8eb23416058836359ce502d045d89511e8e9fd93
Vlad <efimovich94@inbox.ru>
2023-01-15 15:26:10 +0300

    Квадрат числа

---
+++ Example001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напиши программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+
+int num = int.Parse(Console.ReadLine());
+
+int sqr = num * num;
+
+Console.Write("Квадрат числа" + num +" = " + sqr);
