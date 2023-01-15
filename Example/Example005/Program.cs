50efbc7a09f14d9b7492b1737a68c944efce5d79
Vlad <efimovich94@inbox.ru>
2023-01-15 18:18:43 +0300

    Задача №5

---
+++ Example/Example005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int numB = -numA;
+while(numB <= numA)
+{
+    Console.Write($"{numB} ");
+    numB += 1;
+}
